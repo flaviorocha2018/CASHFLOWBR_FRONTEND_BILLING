5a5329d5cc85d40f5fe4db9fd441507b6d84f1a3
Flavio Rocha <f.oliveirarocha2014@gmail.com>
2023-01-24 22:49:49 -0300

    feat: Added react-router-dom V6.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,14 @@
-export { };
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+
+export const AppRoutes = () => {
+
+    return(
+        <Routes>
+            <Route path="/home-page" element={<h2>Home Page</h2>} />
+
+            <Route path= "*" element={<Navigate to="/home-page" />} />
+
+        </Routes>
+    )
+}
